55a279c6acd776d18c2e4b01332f44874b8f454e
emredevops <84643171+emredevops@users.noreply.github.com>
2021-05-23 02:24:49 +0300

    Add files via upload

--- 3. Application Management and Deployment/flask/app.py
+++ 3. Application Management and Deployment/flask/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/home')
+def index():
+   return "Hello. This page created by Emre Oztoprak"
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
